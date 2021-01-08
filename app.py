b9cfe31e7da561aa4768fee7125781e3f6ed4448
tangboxuan <tangboxuan1999@gmail.com>
2021-01-08 23:57:25 +0800

    flask bug

--- app.py
+++ app.py
@@ -110,7 +110,5 @@ def favicon():
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 if __name__ == "__main__":
-    # app.debug = True
-    # port = int(os.environ.get("PORT", 5000))
-    # app.run(host="0.0.0.0", port=port)
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
