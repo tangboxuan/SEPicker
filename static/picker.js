0cb6df6ad5255528e9e77d8043ed36a2e3b84107
tangboxuan <tangboxuan1999@gmail.com>
2021-01-08 19:04:45 +0800

    clear field

--- static/picker.js
+++ static/picker.js
@@ -4,3 +4,51 @@ function toggle(source) {
     checkboxes[i].checked = source.checked;
     }
 }
+
+function clearEM(){
+    var textbox = document.getElementById('em');
+    var collection = textbox.getElementsByTagName('option');
+
+    for(var i = 0; i<collection.length; i++){
+        collection[i].selected = false;
+    }
+
+    var text = document.getElementsByClassName("filter-option-inner-inner");
+    text[0].innerHTML = "<span style='color:#999'>Nothing selected</span>";
+}
+
+function clearOM(){
+    var textbox = document.getElementById('om');
+    var collection = textbox.getElementsByTagName('option');
+
+    for(var i = 0; i<collection.length; i++){
+        collection[i].selected = false;
+    }
+
+    var text = document.getElementsByClassName("filter-option-inner-inner");
+    text[1].innerHTML = "<span style='color:#999'>Nothing selected</span>";
+}
+
+function clearCountry(){
+    var textbox = document.getElementById('countries');
+    var collection = textbox.getElementsByTagName('option');
+
+    for(var i = 0; i<collection.length; i++){
+        collection[i].selected = false;
+    }
+
+    var text = document.getElementsByClassName("filter-option-inner-inner");
+    text[2].innerHTML = "<span style='color:#999'>Nothing selected</span>";
+}
+
+function clearSchool(){
+    var textbox = document.getElementById('schools');
+    var collection = textbox.getElementsByTagName('option');
+
+    for(var i = 0; i<collection.length; i++){
+        collection[i].selected = false;
+    }
+
+    var text = document.getElementsByClassName("filter-option-inner-inner");
+    text[3].innerHTML = "<span style='color:#999'>Nothing selected</span>";
+}
