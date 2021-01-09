a2fab08455bb2161e5087bfe5e52ff3831f659d6
tangboxuan <tangboxuan1999@gmail.com>
2021-01-09 10:26:37 +0800

    heroku

--- app.py
+++ app.py
@@ -257,6 +257,6 @@ def plans():
 
 
 if __name__ == "__main__":
-    # port = int(os.environ.get("PORT", 5000))
-    # app.run(host="0.0.0.0", port=port)
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+    # app.run(debug=True)
